ae5b4f62d6b3e267e3481aa45d6600341db29497
enejivk <enejivic@gmail.com>
2024-03-06 15:55:40 +0100

    my first javascript project

---
+++ apps.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector('.btn');
+const main = document.querySelector('.changeColor');
+
+function getRgb(){
+  
+    let red = Math.floor(Math.random()*255);
+    let green = Math.floor(Math.random()*255);
+    let black = Math.floor(Math.random()*255);
+    return `rgb(${red}, ${green}, ${black})`
+}
+function changeColor(){
+let color = getRgb();
+const colorName = document.querySelector('.colorName');
+colorName.textContent = color;
+main.style.background = color;
+}
+btn.addEventListener('click', changeColor);
